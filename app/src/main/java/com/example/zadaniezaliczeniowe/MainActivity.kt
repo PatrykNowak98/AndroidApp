3ac3ba9f0240b98cc3b402422318eb08a20894c4
Patryk Nowak <patryknowak@intmail.pl>
2022-11-17 14:32:08 +0100

    Moving through activities

--- app/src/main/java/com/example/zadaniezaliczeniowe/MainActivity.kt
+++ app/src/main/java/com/example/zadaniezaliczeniowe/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.zadaniezaliczeniowe
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttonOne = findViewById<Button>(R.id.button)
+        buttonOne.setOnClickListener{
+            val intent = Intent(this,Stew::class.java)
+            startActivity(intent)
+        }
     }
 }
